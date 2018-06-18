707150ddf11f347729339fcd630fd9ede9e7c18e
rassch <karol.schewe@gmail.com>
2018-06-19 00:43:39 +0200

    made matrices H and S TODO: eigenvalues

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from statistics import variance
-
-import sympy
-#from scipy.stats import alpha_gen
-from mpmath import psi
-from sympy import Symbol, integrate, Derivative, init_printing, symbols, solve, diff, exp,Abs, oo , Matrix,ones, simplify
+from sympy import Symbol, diff, exp,Abs
 from scipy.optimize import minimize
 import scipy
 import numpy as np
-from scipy import  integrate
+from scipy import integrate
+from scipy.misc import derivative
 
 #sympy.init_printing(use_latex="mathjax")
 
@@ -41,7 +37,7 @@ A2 = -6.28
 
 h_c = 1970#A*eV
 
-
+stala_hamilton = ((-h_c**2)/(2*m_c_squared))
 
 R = Symbol("R")
 
@@ -188,184 +184,176 @@ def ext_potential(variable):
 #psi --nie usuwaj mi tych tablic z psi
 
 #psi_tab =[U(X1,"a")*U(X2,"b"),U(X2,"a")*U(X1,"b"),U(X1,"a")*U(X2,"a"),U(X1,"b")*U(X2,"b")]
+def psi_w_a(x):
+    return ((alpha1 ** 1 / 2) / pi ** 1 / 4) * np.exp(-alpha1 ** 2 * (x - XWellA) ** 2 / 2)
+def psi_w_b(x):
+    return ((alpha1**1/2)/pi**1/4)*np.exp(-alpha1**2*(x-XWellB)**2/2)
+
+
+
 def psiAB(x,y):
-    return ((alpha1**1/2)/pi**1/4)*np.exp(-alpha1**2*(x-XWellA)**2/2)*((alpha1**1/2)/pi**1/4)*np.exp(-alpha1**2*(y-XWellB)**2/2)
+    return psi_w_a(x)*psi_w_b(y)
 def psiBA(x,y):
-    return ((alpha1 ** 1 / 2) / pi ** 1 / 4) * np.exp(-alpha1 ** 2 * (y - XWellA) ** 2 / 2) * (
-                (alpha1 ** 1 / 2) / pi ** 1 / 4) * np.exp(-alpha1 ** 2 * (x - XWellB) ** 2 / 2)
+    return psi_w_a(y)*psi_w_b(x)
 
 def psiAA(x,y):
-    return ((alpha1 ** 1 / 2) / pi ** 1 / 4) * np.exp(-alpha1 ** 2 * (x - XWellA) ** 2 / 2) * (
-            (alpha1 ** 1 / 2) / pi ** 1 / 4) * np.exp(-alpha1 ** 2 * (y - XWellA) ** 2 / 2)
+    return psi_w_a(x)*psi_w_a(y)
 
 def psiBB(x,y):
-    return ((alpha1 ** 1 / 2) / pi ** 1 / 4) * np.exp(-alpha1 ** 2 * (x - XWellB) ** 2 / 2) * (
-                (alpha1 ** 1 / 2) / pi ** 1 / 4) * np.exp(-alpha1 ** 2 * (y - XWellB) ** 2 / 2)
+    return psi_w_b(x)*psi_w_b(y)
+
+
 
 def hamiltonianAA(x,y):
-    return ((-h_c**2)/(2*m_c_squared))*diff(psiAA(x,y),x,x)+((-h_c**2)/(2*m_c_squared))*diff(psiAA,y,y)+(internal_potential(x,y)+ext_potential(x)+ext_potential(y))
-
-def hamiltonianAB(x, y):
-    return double_derivative(psiAB(x, y), x) + double_derivative(psiAB(x, y), y) + (
-                    internal_potential(x, y) + ext_potential(x) + ext_potential(y))
-
-def hamiltonianBA(x, y):
-    return double_derivative(psiBA(x, y), x) + double_derivative(psiBA(x, y), y) + internal_potential(x, y) + ext_potential(x) + ext_potential(y)
-
-def hamiltonianBB(x, y):
-    return double_derivative(psiBB(x, y), x) + double_derivative(psiBB(x, y), y) + (internal_potential(x, y) + ext_potential(x) + ext_potential(y))
+    return stala_hamilton*derivative(psi_w_a,x,n=2,dx=10**-6)*psi_w_a(y)+stala_hamilton*derivative(psi_w_a,y,n=2,dx=10**-6)*psi_w_a(x)+internal_pot(x,y)+ext_potential(x)+ext_potential(y)
+def hamiltonianBB(x,y):
+    return stala_hamilton*derivative(psi_w_b,x,n=2,dx=10**-6)*psi_w_b(y)+stala_hamilton*derivative(psi_w_b,y,n=2,dx=10**-6)*psi_w_b(x)+internal_pot(x,y)+ext_potential(x)+ext_potential(y)
+def hamiltonianAB(x,y):
+    return stala_hamilton*derivative(psi_w_a,x,n=2,dx=10**-6)*psi_w_b(y)+stala_hamilton*derivative(psi_w_b,y,n=2,dx=10**-6)*psi_w_a(x)+internal_pot(x,y)+ext_potential(x)+ext_potential(y)
+def hamiltonianBA(x,y):
+    return stala_hamilton*derivative(psi_w_b,x,n=2,dx=10**-6)*psi_w_a(y)+stala_hamilton*derivative(psi_w_a,y,n=2,dx=10**-6)*psi_w_b(x)+internal_pot(x,y)+ext_potential(x)+ext_potential(y)
 
 
-# Wektor C
-#print('jeszcze raz internal')
-#print(internal_potential)
-#print('ext1')
-#print(ext_potential(X1))
-#print('druga pochodna')
-#print(double_derivative(psi_tab[0],X1))
+
+#i =1 AB; i =2 BA; i = 3 AA ;i =4 BB
+b = np.array([[0., 1.], [1., 1.]])
+def S11 (x,y):
+    return psiAB(x,y)*psiAB(x,y)
+def S12 (x,y):
+    return psiAB(x,y)*psiBA(x,y)
+def S13 (x,y):
+    return psiAB(x,y)*psiAA(x,y)
+def S14 (x,y):
+    return psiAB(x,y)*psiBB(x,y)
+def S21 (x,y):
+    return psiBA(x,y)*psiAB(x,y)
+def S22 (x,y):
+    return psiBA(x,y)*psiBA(x,y)
+def S23 (x,y):
+    return psiBA(x,y)*psiAA(x,y)
+def S24 (x,y):
+    return psiBB(x,y)*psiBB(x,y)
+def S31 (x,y):
+    return psiAA(x,y)*psiAB(x,y)
+def S32 (x,y):
+    return psiAA(x,y)*psiBA(x,y)
+def S33 (x,y):
+    return psiAA(x,y)*psiAA(x,y)
+def S34 (x,y):
+    return psiAA(x,y)*psiBB(x,y)
+def S41 (x,y):
+    return psiBB(x,y)*psiAB(x,y)
+def S42 (x,y):
+    return psiBB(x,y)*psiBA(x,y)
+def S43 (x,y):
+    return psiBB(x,y)*psiAA(x,y)
+def S44 (x,y):
+    return psiBB(x,y)*psiBB(x,y)
+
+def h11 (x,y):
+    return psiAB(x,y)*hamiltonianAB(x,y)
+def h12 (x,y):
+    return psiAB(x,y)*hamiltonianBA(x,y)
+def h13 (x,y):
+    return psiAB(x,y)*hamiltonianAA(x,y)
+def h14 (x,y):
+    return psiAB(x,y)*hamiltonianBB(x,y)
+def h21 (x,y):
+    return psiBA(x,y)*hamiltonianAB(x,y)
+def h22 (x,y):
+    return psiBA(x,y)*hamiltonianBA(x,y)
+def h23 (x,y):
+    return psiBA(x,y)*hamiltonianAA(x,y)
+def h24 (x,y):
+    return psiBB(x,y)*hamiltonianBB(x,y)
+def h31 (x,y):
+    return psiAA(x,y)*hamiltonianAB(x,y)
+def h32 (x,y):
+    return psiAA(x,y)*hamiltonianBA(x,y)
+def h33 (x,y):
+    return psiAA(x,y)*hamiltonianAA(x,y)
+def h34 (x,y):
+    return psiAA(x,y)*hamiltonianBB(x,y)
+def h41 (x,y):
+    return psiBB(x,y)*hamiltonianAB(x,y)
+def h42 (x,y):
+    return psiBB(x,y)*hamiltonianBA(x,y)
+def h43 (x,y):
+    return psiBB(x,y)*hamiltonianAA(x,y)
+def h44 (x,y):
+    return psiBB(x,y)*hamiltonianBB(x,y)
+
+print("liczę S11")
+w_S11 = scipy.integrate.dblquad(S11,-2,4,lambda x:0, lambda x:6)
+print("liczę S12")
+w_S12 = scipy.integrate.dblquad(S12,-2,4,lambda x:0, lambda x:6)
+print("liczę S13")
+w_S13 = scipy.integrate.dblquad(S13,-2,4,lambda x:0, lambda x:6)
+print("liczę S14")
+w_S14 = scipy.integrate.dblquad(S14,-2,4,lambda x:0, lambda x:6)
+print("liczę S21")
+w_S21 = scipy.integrate.dblquad(S21,-2,4,lambda x:0, lambda x:6)
+print("liczę S22")
+w_S22 = scipy.integrate.dblquad(S22,-2,4,lambda x:0, lambda x:6)
+print("liczę S23")
+w_S23 = scipy.integrate.dblquad(S23,-2,4,lambda x:0, lambda x:6)
+print("liczę S24")
+w_S24 = scipy.integrate.dblquad(S24,-2,4,lambda x:0, lambda x:6)
+print("liczę S31")
+w_S31 = scipy.integrate.dblquad(S31,-2,4,lambda x:0, lambda x:6)
+print("liczę S32")
+w_S32 = scipy.integrate.dblquad(S32,-2,4,lambda x:0, lambda x:6)
+print("liczę S33")
+w_S33 = scipy.integrate.dblquad(S33,-2,4,lambda x:0, lambda x:6)
+print("liczę S34")
+w_S34 = scipy.integrate.dblquad(S34,-2,4,lambda x:0, lambda x:6)
+print("liczę S41")
+w_S41 = scipy.integrate.dblquad(S41,-2,4,lambda x:0, lambda x:6)
+print("liczę S42")
+w_S42 = scipy.integrate.dblquad(S42,-2,4,lambda x:0, lambda x:6)
+print("liczę S43")
+w_S43 = scipy.integrate.dblquad(S43,-2,4,lambda x:0, lambda x:6)
+print("liczę S44")
+w_S44 = scipy.integrate.dblquad(S44,-2,4,lambda x:0, lambda x:6)
+
+print("liczę h11")
+w_h11 = scipy.integrate.dblquad(h11,-2,4,lambda x:0, lambda x:6)
+print("liczę h12")
+w_h12 = scipy.integrate.dblquad(h12,-2,4,lambda x:0, lambda x:6)
+print("liczę h13")
+w_h13 = scipy.integrate.dblquad(h13,-2,4,lambda x:0, lambda x:6)
+print("liczę h14")
+w_h14 = scipy.integrate.dblquad(h14,-2,4,lambda x:0, lambda x:6)
+print("liczę h21")
+w_h21 = scipy.integrate.dblquad(h21,-2,4,lambda x:0, lambda x:6)
+print("liczę h22")
+w_h22 = scipy.integrate.dblquad(h22,-2,4,lambda x:0, lambda x:6)
+print("liczę h23")
+w_h23 = scipy.integrate.dblquad(h23,-2,4,lambda x:0, lambda x:6)
+print("liczę h24")
+w_h24 = scipy.integrate.dblquad(h24,-2,4,lambda x:0, lambda x:6)
+print("liczę h31")
+w_h31 = scipy.integrate.dblquad(h31,-2,4,lambda x:0, lambda x:6)
+print("liczę h32")
+w_h32 = scipy.integrate.dblquad(h32,-2,4,lambda x:0, lambda x:6)
+print("liczę h33")
+w_h33 = scipy.integrate.dblquad(h33,-2,4,lambda x:0, lambda x:6)
+print("liczę h34")
+w_h34 = scipy.integrate.dblquad(h34,-2,4,lambda x:0, lambda x:6)
+print("liczę h41")
+w_h41 = scipy.integrate.dblquad(h41,-2,4,lambda x:0, lambda x:6)
+print("liczę h42")
+w_h42 = scipy.integrate.dblquad(h42,-2,4,lambda x:0, lambda x:6)
+print("liczę h43")
+w_h43 = scipy.integrate.dblquad(h43,-2,4,lambda x:0, lambda x:6)
+print("liczę h44")
+w_h44 = scipy.integrate.dblquad(h44,-2,4,lambda x:0, lambda x:6)
+
+matrixS = np.array([[w_S11[0],w_S12[0],w_S13[0],w_S14[0]],[w_S21[0],w_S22[0],w_S23[0],w_S24[0]],[w_S31[0],w_S32[0],w_S33[0],w_S34[0]],[w_S41[0],w_S42[0],w_S43[0],w_S44[0]]])
+print(matrixS)
+matrixH = np.array([[w_h11[0],w_h12[0],w_h13[0],w_h14[0]],[w_h21[0],w_h22[0],w_h23[0],w_h24[0]],[w_h31[0],w_h32[0],w_h33[0],w_h34[0]],[w_h41[0],w_h42[0],w_h43[0],w_h44[0]]])
+print(matrixH)
+
 
 c_Vector = [C1,C2,C3,C4]
-#print('hamiltionian')
-#print(hamiltonian(psi_tab[0],psi_tab[1],X1,X2))
-caleczka = scipy.integrate.dblquad(hamiltonianAA,0,1,lambda x:0, lambda x:1)
-print(caleczka)
 
-'''
-H_Matrix = []
-for i in range(0,3):
-    new = []
-    for j in range(0,3):
-        print('cos sie robi')
-        new.append(scipy.integrate.dblquad(lambda X1,X2: hamiltonian(psi_tab[i],psi_tab[j],X1,X2),(X1,XWellA-3,XWellA+3),(X2,XWellB-3,XWellB+3)))
-    H_Matrix.append(new)
-print(H_Matrix)
-
-S_Matrix = []
-for i in range(0,3):
-    new2 = []
-    for j in range(0,3):
-        print('cos sie robi')
-        new2.append(scipy.integrate.dblquad(lambda X1,X2: hamiltonian(psi_tab[i],psi_tab[j],X1,X2),(X1,XWellA-3,XWellA+3),(X2,XWellB-3,XWellB+3)))
-    C_Matrix.append(new2)
-print(C_Matrix)
-
-
-'''
-'''
-print('Test')
-print(U(X1,"a")*U(X2,"b"))
-print()
-test2 = integrate(psi_tab[0],(X1,XWellA-3,XWellA+3))*integrate(psi_tab[0],(X2,XWellB-3,XWellB+3))
-print("wynik testu")
-print(simplify(test2))
-#h1 = integrate(psi_tab[0]*hamiltonian(psi_tab[0],X1,X2),X1)*integrate(psi_tab[0]*hamiltonian(psi_tab[0],X1,X2),X2)
-#print(h1)
-print(hamiltonian(psi_tab[0],psi_tab[1],X1,X2))
-
-calka_ham =  integrate(hamiltonian(psi_tab[0],psi_tab[1],X1,X2),X1)*integrate(hamiltonian(psi_tab[0],psi_tab[1],X1,X2),X2)
-print(calka_ham)
-
-
-
-
-
-
-
-
-
-
-
-psi = test
-
-print(test)
-
-psi = psi.subs(x,1)
-
-psi_squared = psi*psi
-
-do_h_psi = hamiltonian(psi)
-
-h_psi = psi * do_h_psi
-
-print("podstawianie: ")
-
-suma = []
-
-for i in range(0,6,1):
-
-    suma.append(0)
-
-całka_h = 0
-
-for i in range(0,6,1): #musi być int
-
-    for j in range(0, 6, 1):
-
-        suma[j] = suma[j] +((1/2)*(h_psi.subs([(X1,i),(X2,j)])+h_psi.subs([(X1,i+1),(X2,j)])))
-
-
-
-print("całka z h_psi")
-
-for i in range(0,5,1): #musi być int
-
-    całka_h = całka_h + ((1/2)*(suma[i]+suma[i+1]))
-
-#function_to_optimize = integrate(h_psi,(X1,-3/sympy.sqrt(alpha1),3/sympy.sqrt(alpha1)),(X2,-3/sympy.sqrt(alpha2),3/sympy.sqrt(alpha2)))/integrate(psi_squared,(X1,-3/sympy.sqrt(alpha1),3/sympy.sqrt(alpha1)),(X2,-3/sympy.sqrt(alpha2),3/sympy.sqrt(alpha2)))#FUNKCJA INTEGRATE NIE WSPÓŁPRACUJE
-
-
-
-#print(function_to_optimize)
-
-print(całka_h)
-
-
-
-
-
-całka = 0
-
-for i in range(0,6,1): #musi być int
-
-    for j in range(0, 6, 1):
-
-        suma[j] = suma[j] +((1/2)*(psi_squared.subs([(X1,i),(X2,j)])+psi_squared.subs([(X1,i+1),(X2,j)])))
-
-
-
-print("całka z bez h:")
-
-for i in range(0,5,1): #musi być int
-
-    całka = całka + ((1/2)*(suma[i]+suma[i+1]))
-
-
-
-print(całka)
-
-
-
-def f(alfa):
-
-    return całka_h.subs([(alpha1,alfa[0]),(alpha2,alfa[1])])/całka.subs([(alpha1,alfa[0]),(alpha2,alfa[1])])
-
-
-
-# Stałe C1... chyba juz niepotrzebne...
-
-
-
-#nie potrafię podstawić do funkcji fmin funkcji z dwoma zmiennymi
-x0 = [3,2]
-print("optymalizacja")
-sol = minimize(f,x0)
-print(sol)
-
-
-
-
-
-
-#należy znaleźć minimum
-
-'''
